8a156b2bc970a845b0e8f6df4d2ba6a321cd3a8c
Learpcs <34576126+Learpcs@users.noreply.github.com>
2020-07-18 04:26:36 +0400

    Update Source.cpp
    Rework BigInteger library into template

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <limits>
+#include <algorithm>
 using namespace std;
 
+	
+template <class type> 
 class BigInteger
 {
 private:
-	size_t base = 10;//65536; //беру такую систему счисления потому, что это крайнее число для того, чтобы при перемножении числа уместились в size_t (если выделяется 4 байта)
-	vector<size_t> digits;
-	bool is_positive = true;	
+	const type base = 1 << sizeof(type) * 4;
+	const type max = base - 1;
+	vector<type> digits;
 
 public:
 	BigInteger()
 	{
-
+		this->digits.push_back(0);
 	}
 
 	BigInteger& operator= (const BigInteger& another)
@@ -23,19 +24,19 @@ public:
 		return *this;
 	}
 
-	BigInteger& operator +=(const BigInteger& another) 
+	BigInteger& operator +=(const BigInteger& another)
 	{
 		while (this->digits.size() < another.digits.size())
 			this->digits.push_back(0);
 		this->digits.push_back(0); //999 <=> 0999
-		size_t size = another.digits.size();                //в два прохода
-		for (size_t i = 0; i < size; i++)                     
+		size_t size = another.digits.size();                //addition in two steps:
+		for (size_t i = 0; i < size; i++)					
 		{
-			this->digits[i] += another.digits[i];			//1 проход - просто складывает
+			this->digits[i] += another.digits[i];			//1 simple addition, ignoring overflow of digits
 		}
 		for (size_t i = 0; ; i++)
 		{
-			if (this->digits[i] >= base)					//2 проход - учитывает остатки				
+			if (this->digits[i] >= base)					//2 take into account the remainders				
 			{
 				this->digits[i] -= base;
 				digits[i + 1]++;
@@ -43,76 +44,66 @@ public:
 			else if (i >= size)
 				break;
 		}
-		while (this->digits.back() == 0 && this->digits.size() != 1) //1 условие - убрать ведущие нули, 2 условие - случай 0 + 0
+		while (this->digits.back() == 0 && this->digits.size() != 1) 
 			this->digits.pop_back();
 		return *this;
-		
 	}
+
 	BigInteger operator +(const BigInteger& another)
 	{
-		*this += another;
-		return *this;
-	}
-	BigInteger& operator -=(const BigInteger& another)
-	{
-
-	}
-	BigInteger& operator -(const BigInteger& another)
-	{
-		*this -= another;
-		return *this;
-	}
-	BigInteger& operator *=(const BigInteger& another)
-	{
-
-	}
-	BigInteger& operator *(const BigInteger& another)
-	{
-		*this *= another;
-		return *this;
-	}
-	BigInteger& operator /=(const BigInteger& another)
-	{
-
-	}
-	BigInteger& operator /(const BigInteger& another)
-	{
-		*this /= another;
-		return *this;
+		BigInteger copy = *this;
+		return copy;
 	}
 
+	template <class type>
+	friend istream& operator >>(istream& in, BigInteger<type>& object);
 
-	
-
-	friend ostream& operator << (ostream& out, const BigInteger& object);
-	friend istream& operator >> (istream& in, BigInteger& object);
+	template <class type>
+	friend ostream& operator <<(ostream& out, const BigInteger<type>& object);
 };
 
-ostream& operator << (ostream& out, const BigInteger& object)
+template <class type>
+istream& operator >>(istream& in, BigInteger<type>& object)
 {
-	for (vector<size_t>::const_reverse_iterator i = object.digits.rbegin(); i != object.digits.rend(); i++)
-		out << *i;
-	return out;
-}
-
-istream& operator >> (istream& in, BigInteger& object)
-{
-	string input;
-	in >> input;
-	//в случае если экземпляр BigInteger уже имеет в себе числа, чтобы они не вводились "поверх" остальных - clear(), т.к. используется push_back()
-	object.digits.clear(); 
-
-	for (string::reverse_iterator i = input.rbegin(); i != input.rend(); i++)
+	object.digits.clear();
+	for (char ch = in.get(); ch != '\n'; ch = in.get())
 	{
-		object.digits.push_back(*i - '0');
+		object.digits.push_back(ch - '0');
+	}
+	size_t size = object.digits.size();
+	//i was doing it at 4 am and i suspect i wanted to do conversion from decimal to my base numerical system 
+	for (size_t i = 1; i < size; i++) //what is this?
+	{
+		for (size_t j = i; j > 0 && object.digits[j - 1] < object.max; j--)
+		{
+			if (object.max - object.digits[j - 1] >= object.digits[j])
+			{
+				object.digits[j - 1] += object.digits[j];
+				object.digits[j] = 0;
+			}
+			else
+			{
+				object.digits[j - 1] += min(object.max - object.digits[j - 1], object.digits[j]);
+				object.digits[j] -= min(object.max - object.digits[j - 1], object.digits[j]);
+			}
+		}
 	}
 	return in;
 }
 
+template <class type>
+ostream& operator <<(ostream& out, const BigInteger<type>& object)
+{
+	for (vector<type>::const_reverse_iterator i = object.digits.rbegin(); i != object.digits.rend(); i++)
+		out << *i;
+	return out;
+}
+
+typedef BigInteger<size_t> BigInt;
+
 int main()
 {
-
-	BigInteger a, b;
+	BigInt a, b;
 	cin >> a >> b;
 	cout << a + b;
 }
